5293feb063c23a52bd12afdcbb02a9bd14df8de3
Sabrina Ford <FordSabrinaR+deere@JohnDeere.com>
2026-05-07 08:50:08 -0300

    aula 01 - vídeo 02 - dinâmica 05

--- Aulas/Aula01/dinamicas/dinamica05.js
+++ Aulas/Aula01/dinamicas/dinamica05.js
@@ -10,3 +10,20 @@ Conferindo o dígito verificador: 2+1+0+0+4+5+3+7 = 22 → 2+2 = 4
 Escreva um programa que solicita ao usuário um número de matrícula sem o dígito
 verificador e imprime na tela o número de matrícula com o dígito verificador. */
 
+import promptsync from "prompt-sync";
+const prompt = promptsync({ sigint: true });
+
+let numeroMatricula = prompt("Insira seu número de matrícula sem o dígito verificador: ");
+let digitos = numeroMatricula;
+let soma = 0;
+
+while (digitos.length != 1) {
+    for (let i = 0; i < digitos.length; i++) {
+        soma = soma + Number(digitos.at(i));
+    }
+    digitos = String(soma)
+    soma = 0;
+}
+
+numeroMatricula = numeroMatricula + '-' + digitos;
+console.log(`Número de mátricula com dígito verificador:  ${numeroMatricula}`)
